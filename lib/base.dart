624c04c65fb1dd4c3ae356462792b462b38b0dca
DaanMoura <daniel.lucio.moura@gmail.com>
2019-04-17 20:49:54 -0300

    melhorando navegação do drawer

--- lib/base.dart
+++ lib/base.dart
@@ -4,14 +4,6 @@ import 'package:gradient_app_bar/gradient_app_bar.dart';
 import 'package:app_secomp/routes/home.dart';
 import 'package:app_secomp/routes/blank.dart';
 
-const HOME = 1;
-const PARTICIPANTE = 2;
-const CRONOGRAMA = 3;
-const EQUIPE = 4;
-const PATRIOCINADORES = 5;
-const DICAS = 6;
-const SOBRE = 7;
-
 class Base extends StatefulWidget {
   @override
   _BaseState createState() => _BaseState();
@@ -23,20 +15,11 @@ class _BaseState extends State<Base> {
   @override
   Widget build(BuildContext context) {
 
-    void _updateBody(int bodyNumber) {
-      switch (bodyNumber) {
-        case HOME:
-          setState(() {
-            _body = HomePage();
+    void _updateBody(Widget body) {
+      setState(() {
+            _body = body;
           });
-          Navigator.pop(context);
-          break;
-        default:
-          setState((){
-            _body = BlankPage();
-          });
-          Navigator.pop(context);
-      }
+      Navigator.pop(context);
     }
 
     Drawer _drawer = Drawer(
@@ -48,37 +31,37 @@ class _BaseState extends State<Base> {
           ListTile(
             title: Text("Home"),
             leading: Icon(Icons.home),
-            onTap: () => _updateBody(HOME),
+            onTap: () => _updateBody(HomePage()),
           ),
           ListTile(
             title: Text("Página do Participante"),
             leading: Icon(Icons.person),
-            onTap: () => _updateBody(0),
+            onTap: () => _updateBody(BlankPage()),
           ),
           ListTile(
             title: Text("Cronograma"),
             leading: Icon(Icons.calendar_view_day),
-            onTap: () => _updateBody(0),
+            onTap: () => _updateBody(BlankPage()),
           ),
           ListTile(
             title: Text("Equipe"),
             leading: Icon(Icons.people),
-            onTap: () => _updateBody(0),
+            onTap: () => _updateBody(BlankPage()),
           ),
           ListTile(
             title: Text("Patriocinadores"),
             leading: Icon(Icons.star),
-            onTap: () => _updateBody(0),
+            onTap: () => _updateBody(BlankPage()),
           ),
           ListTile(
             title: Text("Dicas"),
             leading: Icon(Icons.lightbulb_outline),
-            onTap: () => _updateBody(0),
+            onTap: () => _updateBody(BlankPage()),
           ),
           ListTile(
             title: Text("Sobre"),
             leading: Icon(Icons.info),
-            onTap: () => _updateBody(0),
+            onTap: () => _updateBody(BlankPage()),
           ),
         ],
       ),
