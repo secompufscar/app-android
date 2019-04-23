d744471ed1622cd86597f3283ba2aca052cf51aa
DaanMoura <daniel.lucio.moura@gmail.com>
2019-04-23 18:35:14 -0300

    cor padrão: Cyan

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class AppSecomp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
         fontFamily: 'Lato',
       ),
       home: Base(),
