07941efe40423c1fe78974149bceb8c62a0a87e9
DaanMoura <daniel.lucio.moura@gmail.com>
2019-05-02 18:07:34 -0300

    terminando primeira página Sobre. Adicionado ícones personalizados.

--- lib/components/text_button.dart
+++ lib/components/text_button.dart
@@ -4,8 +4,10 @@ class TextButton extends StatelessWidget {
   final String text;
   final Function action;
   final TextStyle style;
+  final Icon icon;
+  final bool center;
 
-  TextButton(this.text, {this.action, this.style});
+  TextButton(this.text, {this.action, this.style, this.icon, this.center: false});
 
   @override
   Widget build(BuildContext context) {
@@ -20,14 +22,40 @@ class TextButton extends StatelessWidget {
       realStyle = this.style;
     }
 
+    Text textWidget = Text(
+      text,
+      style: realStyle,
+    );
+
+    Widget child;
+    
+    MainAxisAlignment alignment;
+    if(center) alignment = MainAxisAlignment.center;
+    else alignment = MainAxisAlignment.start;
+
+    if (this.icon == null) {
+      child = textWidget;
+    } else {
+      child = Row(
+        mainAxisAlignment: alignment,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: icon,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 2),
+            child: textWidget,
+          )
+        ],
+      );
+    }
+
     return InkWell(
       borderRadius: BorderRadius.circular(10),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Text(
-          text,
-          style: realStyle,
-        ),
+        child: child,
       ),
       onTap: action,
     );
--- lib/routes/sobre.dart
+++ lib/routes/sobre.dart
@@ -1,10 +1,18 @@
 import 'package:app_secomp/components/logo.dart';
 import 'package:app_secomp/components/text_button.dart';
+import 'package:app_secomp/icons/secomp_social_icons.dart';
 import 'package:flutter/material.dart';
 
 class Sobre extends StatelessWidget {
+  final String versao = "1.0.0";
+
   @override
   Widget build(BuildContext context) {
+    final divisao = Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Divider(),
+    );
+
     return ListView(
       padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
       children: <Widget>[
@@ -31,12 +39,10 @@ class Sobre extends StatelessWidget {
             ),
           ),
         ),
-        //DIVISAO:
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: Divider(),
-        ),
+        divisao,
         //LINKS:
+        // os prints são apenas para ter uma action (serve também para testar)
+        // depois serão substituídas pela ação real.
         TextButton(
           "A SECOMP",
           action: () => print("a secomp"),
@@ -49,6 +55,45 @@ class Sobre extends StatelessWidget {
           "A UFSCar",
           action: () => print("a ufscar"),
         ),
+        divisao,
+        TextButton(
+          "Siga-nos no Facebook",
+          style: TextStyle(fontSize: 16),
+          icon: Icon(SecompSocial.facebook_squared),
+          action: () => print("facebook"),
+        ),
+        TextButton(
+          "Siga-nos no Instagram",
+          icon: Icon(SecompSocial.instagram),
+          style: TextStyle(fontSize: 16),
+          action: () => print("instagram"),
+        ),
+        TextButton(
+          "Siga-nos no Twitter",
+          style: TextStyle(fontSize: 16),
+          icon: Icon(SecompSocial.twitter),
+          action: () => print("twitter"),
+        ),
+        divisao,
+        Center(
+          child: Text(
+            "Nosso email está disponível para qualquer dúvida, sugestão ou avaliação. Entre em contato!",
+            style: TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        TextButton(
+          "organizacaosecompufscar@gmail.com",
+          style: TextStyle(fontSize: 16),
+          icon: Icon(Icons.email),
+          center: true,
+          action: () => print("email"),
+        ),
+        Center(
+            child: Text(
+          "Versão $versao",
+          style: TextStyle(color: Colors.grey),
+        ))
       ],
     );
   }
