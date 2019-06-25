707314261bbc254d061b6e019b9953947dc7b38b
igorlmgx <igorlmgx@gmail.com>
2019-06-25 16:25:14 -0300

    Adicionado tipo da atividade

--- lib/models/atividade.dart
+++ lib/models/atividade.dart
@@ -1,6 +1,17 @@
 import 'ministrante.dart';
 
+  enum Tipo {
+    Palestra,
+    Minicurso,
+    Workshop,
+    MesaRedonda,
+    Coffee,
+    Institucional
+  }
+
 class Atividade {
+
+  final Tipo tipo;
   final String titulo;
   final Ministrante ministrante;
   final DateTime inicio;
@@ -8,5 +19,5 @@ class Atividade {
   final String local;
   final String descricao;
 
-  Atividade(this.titulo, this.ministrante, this.inicio, this.fim, this.local, this.descricao);
+  Atividade(this.tipo, this.titulo, this.ministrante, this.inicio, this.fim, this.local, this.descricao);
 }
