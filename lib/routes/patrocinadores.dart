7c98ca4da0415644db836667a7374d61c92b77ea
MFelipe96 <cc.ufscar@gmail.com>
2019-08-19 23:57:23 -0300

    Listagem por categoria e animações funcionando

--- lib/routes/patrocinadores.dart
+++ lib/routes/patrocinadores.dart
@@ -1,6 +1,16 @@
+import 'dart:math';
+
 import 'package:app_secomp/routes/login.dart';
 import 'package:flutter/material.dart';
 import 'package:app_secomp/routes/button_patrocionadores.dart';
+
+final List<String> patrocinadoresPrata = <String> ['https://picsum.photos/id/11/250/250', 'https://picsum.photos/id/11/250/250', 'https://picsum.photos/id/11/250/250', 'https://picsum.photos/id/11/250/250', 'https://picsum.photos/id/11/250/250', 'https://picsum.photos/id/11/250/250', 'https://picsum.photos/id/11/250/250', 'https://picsum.photos/id/11/250/250', 'https://picsum.photos/id/11/250/250'];
+final List<String> patrocinadoresOuro = <String>['https://picsum.photos/id/10/250/250', 'https://picsum.photos/id/10/250/250', 'https://picsum.photos/id/10/250/250', 'https://picsum.photos/id/10/250/250', 'https://picsum.photos/id/10/250/250', 'https://picsum.photos/id/10/250/250', 'https://picsum.photos/id/10/250/250', 'https://picsum.photos/id/10/250/250'];
+final List<String> patrocinadoresDiamente = <String>['https://picsum.photos/250?image=9', 'https://picsum.photos/250?image=9', 'https://picsum.photos/250?image=9', 'https://picsum.photos/250?image=9', 'https://picsum.photos/250?image=9', 'https://picsum.photos/250?image=9'];
+
+  bool _anime = false;
+  bool _animeTwo = true;
+  bool _animeThree = true;
 
 class Patrocinadores extends StatefulWidget{
   @override
@@ -10,10 +20,8 @@ class Patrocinadores extends StatefulWidget{
 
 class _PatrocinadoresState extends State<Patrocinadores>{
 
-  final List<String> entries = <String>['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L'];
-  bool _anime = true;
-  bool _animeTwo = true;
-  bool _animeThree = true;
+    int tam = 0;
+    List<String> listLinks;
 
   _onTap(){
       setState(() {
@@ -21,6 +29,8 @@ class _PatrocinadoresState extends State<Patrocinadores>{
       _animeTwo = true;
       _animeThree = true;
     });
+    tam = patrocinadoresDiamente.length;
+    listLinks = patrocinadoresDiamente;
   }
 
   _onTapTwo(){
@@ -29,6 +39,8 @@ class _PatrocinadoresState extends State<Patrocinadores>{
       _anime = true;
       _animeThree = true;
     });
+    tam = patrocinadoresOuro.length;
+    listLinks = patrocinadoresOuro;
   }
 
   _onTapThree(){
@@ -37,6 +49,8 @@ class _PatrocinadoresState extends State<Patrocinadores>{
       _animeTwo = true;
       _anime = true;
     });
+    tam = patrocinadoresPrata.length;
+    listLinks = patrocinadoresPrata;
   }
 
   @override
@@ -45,39 +59,111 @@ class _PatrocinadoresState extends State<Patrocinadores>{
         body: Container(
           child: Column(
             children: <Widget>[
-            Expanded(
-            child: ButtonPatrocionadores(onTap: _onTap, onTapTwo: _onTapTwo, onTapThree: _onTapThree, 
+               Expanded(
+                  child: ButtonPatrocionadores(onTap: _onTap, onTapTwo: _onTapTwo, onTapThree: _onTapThree, 
                                          anime: _anime, animeTwo: _animeTwo, animeThree: _animeThree,),
-            flex: 1,
-            ),
-            Expanded(
-                child: ListView.separated(
-                itemCount: entries.length,
-                itemBuilder: (BuildContext context, int index) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                  //margin: EdgeInsets.symmetric(vertical: 5, horizontal: 30),       
-                     Container(
-                        height: 100,
-                        width: 170,
-                        child: Center(child: Text('Patrocinador ${entries[index]}')),
-                     ),
-                      Container(
-                        height: 100,
-                        width: 170,
-                        child: Center(child: Text('Patrocinador ${entries[index]}')),
-                      ),
-                  ]
-                );
-            },
-            separatorBuilder: (BuildContext context, int index) => const Divider(height: 0, indent: 0,),
-            ),
-            flex: 6,
-          ),
+                flex: 1,
+              ),
+            inicializacao(tam, listLinks)
             ]
           ),
         ),      
     );
   }
+}
+
+Expanded inicializacao(int tam, List<String> listLinks){
+
+//todo: fazer o loading enquanto carregam as imagens
+
+  if(tam == 0){
+    return Expanded(
+    child: ListView.separated(
+                itemCount: patrocinadoresDiamente.length,
+                itemBuilder: (BuildContext context, int index) { 
+                return Container(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                     Container(
+                        padding: EdgeInsetsDirectional.only(top: 5, bottom: 5),
+                        height: 140,
+                        width: 170,
+                        child: Image.network('${patrocinadoresDiamente[index]}')   
+                     ),
+                      Container(
+                       padding: EdgeInsetsDirectional.only(top: 5, bottom: 5),
+                        height: 140,
+                        width: 170,
+                        child: Image.network('${patrocinadoresDiamente[index]}')
+                      ),
+                  ]
+                ),
+                );
+            },
+            separatorBuilder: (BuildContext context, int index) => const Divider(height: 0, indent: 0,),
+      ),
+    flex: 6,
+    );
+  }else{
+    return Expanded(
+      child: ListView.separated(
+                itemCount: tam,
+                itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                     Container(
+                        padding: EdgeInsetsDirectional.only(top: 5, bottom: 5),
+                        height: 140,
+                        width: 170,
+                        child: Image.network('${listLinks[index]}')   
+                     ),
+                      Container(
+                       padding: EdgeInsetsDirectional.only(top: 5, bottom: 5),
+                        height: 140,
+                        width: 170,
+                        child: Image.network('${listLinks[index]}')
+                      ),
+                  ]
+                ),
+                );
+            },
+            separatorBuilder: (BuildContext context, int index) => const Divider(height: 0, indent: 0,),
+      ),
+      flex: 6,
+            );
+  }
+}
+
+ListView exibirLista(int tam, List<String> listLinks){
+  child: ListView.separated(
+                itemCount: tam,
+                itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                     Container(
+                        padding: EdgeInsetsDirectional.only(top: 5, bottom: 5),
+                        height: 140,
+                        width: 170,
+                        child: Image.network('${listLinks[index]}')   
+                     ),
+                      Container(
+                       padding: EdgeInsetsDirectional.only(top: 5, bottom: 5),
+                        height: 140,
+                        width: 170,
+                        child: Image.network('${listLinks[index]}')
+                      ),
+                  ]
+                ),
+                );
+            },
+            separatorBuilder: (BuildContext context, int index) => const Divider(height: 0, indent: 0,),
+      );
 }
