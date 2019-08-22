d39a49709e19b56bb2a1b70a989e718c7e720d58
DaanMoura <daniel.lucio.moura@gmail.com>
2019-08-21 23:08:54 -0300

    Chamando api de atividades

--- lib/models/atividade.dart
+++ lib/models/atividade.dart
@@ -1,17 +1,9 @@
 import 'ministrante.dart';
 
-  enum Tipo {
-    Palestra,
-    Minicurso,
-    Workshop,
-    MesaRedonda,
-    Coffee,
-    Institucional
-  }
+enum Tipo { Palestra, Minicurso, Workshop, MesaRedonda, Coffee, Institucional }
 
 class Atividade {
-
-  final Tipo tipo;
+  final String tipo;
   final String titulo;
   final Ministrante ministrante;
   final DateTime inicio;
@@ -19,5 +11,22 @@ class Atividade {
   final String local;
   final String descricao;
 
-  Atividade(this.tipo, this.titulo, this.ministrante, this.inicio, this.fim, this.local, this.descricao);
+  Atividade(
+      {this.tipo,
+      this.titulo,
+      this.ministrante,
+      this.inicio,
+      this.fim,
+      this.local,
+      this.descricao});
+
+  factory Atividade.fromJson(Map<String, dynamic> json) {
+    return Atividade(
+      tipo: json['tipo'],
+      titulo: json['titulo'],
+      ministrante: Ministrante(json['ministrantes'][0], "Teste", "TEST"),
+      local: json['local'],
+      descricao: json['descricao'],
+    );
+  }
 }
