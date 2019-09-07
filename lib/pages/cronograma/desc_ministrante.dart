fafdc118c09ad9d8af60261a0ac5875f24cb66c6
igorlmgx <igorlmgx@gmail.com>
2019-09-07 00:07:32 -0300

    Removendo card_ministrante de desc_ministrante

--- lib/pages/cronograma/desc_ministrante.dart
+++ lib/pages/cronograma/desc_ministrante.dart
@@ -1,6 +1,7 @@
 import 'package:app_secomp/components/ministrante_card.dart';
 import 'package:app_secomp/models/ministrante.dart';
 import 'package:flutter/material.dart';
+import 'package:gradient_app_bar/gradient_app_bar.dart';
 
 class MinistrantePage extends StatelessWidget {
   final Ministrante min;
@@ -9,47 +10,92 @@ class MinistrantePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget _buildMinistrante(Ministrante min) {
-      return MinistranteCard(min: min);
-    }
-
-    return Material(
-      color: Colors.white,
-      child: ListView(
-        physics: ClampingScrollPhysics(),
-        shrinkWrap: true,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-            child: Text(
-              "Ministrante",
-              style: TextStyle(
-                fontSize: 24,
+    return Scaffold(
+      appBar: GradientAppBar(title: Text("Ministrante")),
+      body: Material(
+        child: ListView(
+          physics: ClampingScrollPhysics(),
+          shrinkWrap: true,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: ClipOval(
+                    child: Image.network(
+                      "https://secompufscar.com.br/uploads/fotos_ministrantes/" + min.img,
+                      // "https://secompufscar.com.br/static/images/logo-colorida.png",
+                      fit: BoxFit.cover,
+                      width: 110,
+                      height: 110,
+                    ),
+                  ),
+                ),
+                Flexible(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Text(
+                          min.nome,
+                          style: TextStyle(fontSize: 18),
+                          overflow: TextOverflow.clip,
+                          maxLines: 2,
+                          softWrap: true,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 4),
+                        child: Text(
+                          min.cargo,
+                          style: TextStyle(color: Colors.black54),
+                          overflow: TextOverflow.fade,
+                          maxLines: 2,
+                          softWrap: true,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 4),
+                        child: Text(
+                          min.instituicao,
+                          style: TextStyle(color: Colors.black54),
+                          overflow: TextOverflow.fade,
+                          maxLines: 2,
+                          softWrap: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+              child: Text(
+                "Biografia",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
               ),
             ),
-          ),
-          _buildMinistrante(min),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-            child: Text(
-              "Biografia",
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 18,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+              child: Text(
+                min.biografia,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  height: 1.25,
+                  fontSize: 16,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
-            child: Text(
-              min.biografia,
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
